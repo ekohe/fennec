e999a38872e68d14398dcb6bbf413749e20145b2
Jerry Shen <jerry@ekohe.com>
2011-09-20 11:00:24 +0800

    implement navigation helpers and change version to 0.0.8

--- lib/fennec/railtie.rb
+++ lib/fennec/railtie.rb
@@ -1,3 +1,10 @@
+require 'rails'
+require File.join(File.dirname(__FILE__), 'helpers/action_view_extension')
+
 module Fennec
-  class Railtie < ::Rails::Railtie; end
+  class Railtie < ::Rails::Railtie
+    ActiveSupport.on_load(:action_view) do
+      ::ActionView::Base.send :include, Fennec::ActionViewExtension
+    end  
+  end
 end
